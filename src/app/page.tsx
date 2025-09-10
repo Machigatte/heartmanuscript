7de3c0ff7d1ce938ebd9de7a47a17972b7aabe72
Chalkim <jiangyi2012@outlook.com>
2025-09-10 23:05:15 +0800

    refactor: wrap Sidebar and RecordEditor with DataProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { Sidebar } from "@/containers/Sidebar";
 import { RecordEditor } from "@/containers/RecordEditor";
+import { DataProvider } from "@/dataManager";
 
 export default function RecordInterface() {
   return (
     <div className="flex h-screen">
       {/* 左侧历史记录 */}
-      <Sidebar />
+      <DataProvider>
+        <Sidebar />
 
-      {/* 右侧主区域 */}
-      <main className="flex-1 flex flex-col">
-        <RecordEditor />
-      </main>
+        {/* 右侧主区域 */}
+        <main className="flex-1 flex flex-col">
+          <RecordEditor />
+        </main>
+      </DataProvider>
     </div>
   );
 }
