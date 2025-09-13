bfaed4961897e41022a6d7b6da2215bb34cfcff7
Chalkim <jiangyi2012@outlook.com>
2025-09-13 10:14:47 +0800

    feat: 更新API_BASE_URL为生产环境地址

--- src/services/recordService.ts
+++ src/services/recordService.ts
@@ -1,7 +1,7 @@
 import { RecordData } from '../dataManager/types';
 
-// const API_BASE_URL = 'http://jp3.neptunia.net.eu.org:8080';
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'http://jp3.neptunia.net.eu.org:8080';
+// const API_BASE_URL = 'http://localhost:8080';
 
 // 获取所有记录
 export async function fetchAllRecords(): Promise<RecordData[]> {
