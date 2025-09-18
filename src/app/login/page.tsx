baaa9d5b6b8e99013b8c4402a7250d27dbe7acf3
Chalkim <jiangyi2012@outlook.com>
2025-09-18 15:39:26 +0800

    feat: 添加登录页面布局
    - 支持账号密码登录
    - 支持Google/Github等的OAuth2登录

---
+++ src/app/login/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { IconBrandGithub } from "@tabler/icons-react";
+import { IconBrandGoogle } from "@tabler/icons-react";
+
+export default function LoginPage() {
+  // 跳转到 GitHub OAuth2 授权页面
+  const handleGithubLogin = () => {
+    const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
+    const redirectUri = typeof window !== "undefined" ? window.location.origin + "/api/auth/callback" : "";
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user`;
+    window.location.href = githubAuthUrl;
+  };
+
+  return (
+    <Card className="w-full max-w-sm mx-auto mt-24">
+      <CardHeader>
+        <CardTitle>登录 Orinote</CardTitle>
+        <CardDescription>
+          输入您的邮箱及密码以登录您的账户
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">邮箱</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="me@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">密码</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  忘记密码？
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          登录
+        </Button>
+        <div className="flex items-center w-full gap-3 mt-2 mb-2">
+          <Separator className="flex-1" />
+          <div className="text-sm text-muted-foreground">或者</div>
+          <Separator className="flex-1" />
+        </div>
+        <Button variant="outline" className="w-full">
+          <IconBrandGoogle />
+          使用 Google 登录
+        </Button>
+        <Button
+          variant="outline" className="w-full"
+          onClick={handleGithubLogin}
+        >
+          <IconBrandGithub />
+          使用 GitHub 登录
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 "use client";
+import ConfirmDialogRenderer from "@/components/ConfirmDialogRenderer";
 import { Sidebar } from "@/components/Sidebar";
+import { Toaster } from "@/components/ui/sonner";
 import { RecordEditor } from "@/containers/RecordEditor";
+import { ConfirmDialogProvider } from "@/contexts/ConfirmDialogContext";
+import { DataProvider } from "@/dataManager";
 
 export default function RecordInterface() {
   return (
-    <div className="flex h-screen">
-      {/* 左侧历史记录 */}
-      <Sidebar />
+    <DataProvider>
+      <ConfirmDialogProvider>
+        <div className="flex h-screen">
+          {/* 左侧历史记录 */}
+          <Sidebar />
 
-      {/* 右侧主区域 */}
-      <main className="flex-1 flex flex-col">
-        <RecordEditor />
-      </main>
-    </div>
+          {/* 右侧主区域 */}
+          <main className="flex-1 flex flex-col">
+            <RecordEditor />
+          </main>
+        </div>
+
+        <ConfirmDialogRenderer />
+        <Toaster position="top-center" duration={1000} />
+      </ConfirmDialogProvider>
+    </DataProvider>
   );
 }
