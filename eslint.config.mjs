9c8cc4a5b26d3e04367befe8a034e29d8bea901b
Chalkim <jiangyi2012@outlook.com>
2025-11-12 21:24:10 +0800

    chore(eslint): add eslint-plugin-react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactHooks from 'eslint-plugin-react-hooks';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,6 +21,7 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  reactHooks.configs.flat.recommended,
 ];
 
 export default eslintConfig;
